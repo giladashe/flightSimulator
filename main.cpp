2faf995191577d2919db9e4b508709d6da41a4af
shaiyis <shaiyis@gmail.com>
2019-12-12 23:54:15 +0200

    added "Hi Gilad"

--- main.cpp
+++ main.cpp
@@ -3,9 +3,6 @@
 #include "Lexer.h"
 using namespace std;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    std::cout << "HI Gilad";
-    std::cout << "HI Israel shai halevi";
     Lexer lexer;
     ifstream filePointer;
     filePointer.open("/home/giladasher/CLionProjects/flightSimulator/fly.txt",std::ifstream::in);
