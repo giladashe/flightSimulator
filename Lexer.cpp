b656718ca04055bb295d6d908298593dc3ff9583
giladasher <giladash@gmail.com>
2019-12-13 14:10:58 +0200

    fixed bug in lexer

--- Lexer.cpp
+++ Lexer.cpp
@@ -9,6 +9,7 @@
 using namespace std;
 
 vector<string> Lexer::lexer(ifstream &filePointer) {
+    int count = 0;
     vector<string> arrayOfTokens = {};
     if (!filePointer.is_open()) {
         cout << "Error opening file" << endl;
@@ -20,6 +21,7 @@ vector<string> Lexer::lexer(ifstream &filePointer) {
     //flag if the operators need to be together like >=
     bool together = false;
     while (getline(filePointer, line)) {
+        line.erase(remove(line.begin(), line.end(), '\t'), line.end());
         string copyOfLine = string(line);
         string big = string(line);
         string small = string(line);
@@ -31,9 +33,22 @@ vector<string> Lexer::lexer(ifstream &filePointer) {
         //if it's a line like: variable = value
         if (splitLine.size() > 1 && splitLineBig.size() == 1 && splitLineSmall.size() == 1) {
             for (int i = 0; i < splitLine.size(); i++) {
+                if(i==0){
+                    while(splitLine[i][0]==' ')
+                    {
+                        splitLine[i] = splitLine[i].substr(1,splitLine[i].size()-1);
+                    }
+                    if(splitLine[i][0]=='v'){
+                        token = splitLine[i][0];
+                        token+=splitLine[i][1];
+                        token+=splitLine[i][2];
+                        arrayOfTokens.push_back(token);
+                        splitLine[i] = splitLine[i].substr(3,splitLine[i].size()-3);
+                    }
+                }
+
                 //erase spaces - https://stackoverflow.com/questions/83439/remove-spaces-from-stdstring-in-c
                 splitLine[i].erase(remove(splitLine[i].begin(), splitLine[i].end(), ' '), splitLine[i].end());
-                splitLine[i].erase(remove(splitLine[i].begin(), splitLine[i].end(), '\t'), splitLine[i].end());
                 //if it's the variable name
                 if (i == 0) {
                     arrayOfTokens.push_back(splitLine[i]);
@@ -70,7 +85,7 @@ vector<string> Lexer::lexer(ifstream &filePointer) {
             }
         } else {
             for (int i = 0; i < line.length(); i++) {
-                if (line[i] == ' ' || line[i] == ')' || line[i] == '\t') {
+                if (line[i] == ' ' || line[i] == ')') {
                     continue;
                 } else if (isalpha(line[i]) || line[i] == '_') {
                     token = string(1, line[i]);
@@ -91,6 +106,11 @@ vector<string> Lexer::lexer(ifstream &filePointer) {
                         i++;
                     }
                     //except of "print" all expressions in () need to be without spaces
+                    if (arrayOfTokens.back() == "Print") {
+                        count++;
+                        cout << count << endl;
+                    }
+
                     if (arrayOfTokens.back() != "Print") {
                         inParentheses.erase(remove(inParentheses.begin(), inParentheses.end(), ' '),
                                             inParentheses.end());
