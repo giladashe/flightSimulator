15cef2be08e5c6a39d6ba3f88ad0578a4c722072
giladasher <giladash@gmail.com>
2020-01-01 21:16:54 +0200

    added comments

--- Lexer.h
+++ Lexer.h
@@ -23,21 +23,22 @@ public:
 
     static bool isOperator(char token); //checks if char is an operator
 
-    static void removeSpacesFromBegin(string &str);
+    static void removeSpacesFromBegin(string &str); //removes spaces from the begin of the line
 
+    //classify line to assignment line or other
     void classifyLine(string &str, string &firstWord, bool &ifOrWhile, bool &hadFunction, bool &isFunction, bool &PrintOrSleep);
 
-    void insertAssignmentLine(vector<string> splitLine);
+    void insertAssignmentLine(vector<string> splitLine); //insert assignment line to array of tokens
 
     void insertRegularLine(string &line, bool &ifOrWhile, bool &hadFunction, bool &isFunction, bool &PrintOrSleep);
 
-    static bool togetherOperator(char first, char second);
+    static bool togetherOperator(char first, char second); //if operator's size is 2 (like >=) returns true
 
-    int insertVariableCommandAndFunc(string &line, int i);
+    int insertVariableCommandAndFunc(string &line, int i); //insert "word" like to the array of tokens
 
-    int insertNumber(string &line, int i);
+    int insertNumber(string &line, int i); // insert numner to array of tokens
 
-    int insertInsideParentheses(string &line, int i, bool PrintOrSleep);
+    int insertInsideParentheses(string &line, int i, bool PrintOrSleep); //insert the expression inside the parenthesis
 };
 
 //
