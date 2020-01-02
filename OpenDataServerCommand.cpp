bb4fe544742a38e0e94b7788cdbd5216c2eb2da5
shaiyis <shaiyis@gmail.com>
2020-01-02 14:49:30 +0200

    buffer 36

--- OpenDataServerCommand.cpp
+++ OpenDataServerCommand.cpp
@@ -71,76 +71,78 @@ int OpenDataServerCommand::execute(int index, vector<string> &lexer) {
 void serverThread(int client_socket) {
     //reading from client
     auto data = Data::getInstance();
-    string remain; //remain from last read
-    char buffer[1187] = {0};
-    string bufferToString;
-    vector<string> valuesLines;
+    vector<string> xmlVariables = data->getXmlVariables();
+    string between_lines = "";
 
     while (!data->isStop()) {
         // initialize buffer to 0
-        int i;
-        for (i = 0; i < 1187; i++) {
-            buffer[i] = 0;
-        }
+        char buffer[1187] = {0};
 
         // read
-        read(client_socket, buffer, 1187);
-
-        //if the string is not empty it clears it
-        if (bufferToString.length() != 0) {
-            bufferToString.clear();
+        int readData = read(client_socket, buffer, 1187);
+        int i = 0;
+        while( i < readData){
+            if (buffer[i] == '\n'){
+                vector<string> splittedByCom = Lexer::splitByDelimiter(between_lines, ",");
+                // updating the maps
+                int k;
+                for (k = 0; k < (int) splittedByCom.size(); k++) {
+                    //update the key in simMap
+                    string s = splittedByCom[k];
+                    if (!s.empty()) {
+                        data->setValueSimMap(xmlVariables[k], stod(s));
+                        //if there is a bind between the maps it updates the value of the varData in progMap
+                        if ((data->getBindFromSimMap(xmlVariables[k]) == 1)) {
+                            data->setValueProgMap(data->getProgFromSimMap(xmlVariables[k]), (stod(s)));
+                        }
+                    }
+                }
+                between_lines = "";
+                i++;
+                continue;
+            }
+            between_lines+= buffer[i];
+            i++;
         }
 
-        bufferToString = buffer;
+        //if the string is not empty it clears it
+        /*if (bufferToString.length() != 0) {
+            bufferToString.clear();
+        }*/
+
+        //bufferToString = buffer;
 
         //if there were values from previous read it concatenates them to this read
-        if (remain.length() > 0) {
+        /*if (remain.length() > 0) {
             remain += bufferToString;
             bufferToString = remain;
             remain = "";
-        }
+        }*/
 
         // if there is a remain in valueLines - clear
-        if (!valuesLines.empty()) {
+        /*if (!valuesLines.empty()) {
             valuesLines.clear();
-        }
+        }*/
 
         //split the read to lines
-        valuesLines = Lexer::splitByDelimiter(bufferToString, "\n");
+        /*valuesLines = Lexer::splitByDelimiter(bufferToString, "\n");
         if (valuesLines[valuesLines.size() - 1].empty()) {
             valuesLines.pop_back();
-        }
+        }*/
 
         // loop on the lines that sets values in the maps
-        int j = 0;
+        /*int j = 0;
         for (; j < (int) valuesLines.size(); j++) {
             //save the line in different string because split changes it
             string justInCase = valuesLines[j];
-            vector<string> values = Lexer::splitByDelimiter(valuesLines[j], ",");
             // if it's not enough variables(36) put all the values on the side for next iteration
             if (values.size() != 36) {
                 remain += justInCase;
                 break;
             }
-            int k;
+            int k;*/
 
             // variables is _xmlVariables
-            vector<string> variables = data->getXmlVariables();
-
-
-            for (k = 0; k < (int) values.size(); k++) {
-                //update the key in simMap
-
-                string s = values[k];
-                if (!s.empty()) {
-                    data->setValueSimMap(variables[k], stod(s));
-                    //if there is a bind between the maps it updates the value of the varData in progMap
-                    if ((data->getBindFromSimMap(variables[k]) == 1)) {
-                        data->setValueProgMap(data->getProgFromSimMap(variables[k]), (stod(s)));
-                    }
-                }
-            }
         }
-    }
     close(client_socket);
 }
